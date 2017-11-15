fced5f192f182830e8fb297022a515840cfb5e7c
catrionameriel <catriona.meriel@gmail.com>
2017-11-15 14:29:56 +0000

    SQL runner implemented

---
+++ db/sql_runner.rb
@@ -0,0 +1,12 @@
+class SqlRunner
+
+  def self.run(sql, values = [])
+    begin
+      db = PG.connect( {dbname: 'music_collection', host: 'localhost'} )
+      db.prepare('quesry', sql)
+      result = db.exec_prepared('quesry', values)
+    ensure
+      db.close if db != nil
+  end
+  return result
+end
